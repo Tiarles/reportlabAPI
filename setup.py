d20c0ae754058c5df138b559fde1462f5620aa70
Tiarles Guterres <tiarlesmoralles@hotmail.com>
2019-09-28 01:33:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
    packages=find_packages(where="src"),
    package_dir={"": "src"},
    install_requires=["pdfrw", "reportlab"],
-   include_package_data=True
+   include_package_data=True,
+   package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.ttf']}
 )
